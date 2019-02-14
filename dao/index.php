36ec4c0b3d32d41fbf4b5be7d0f0a51ae72f527c
= <=lucasferreiragalvaoti@gmail.com>
2019-02-13 22:34:15 -0200

    Métodos de Lista

--- dao/index.php
+++ dao/index.php
@@ -1,8 +1,22 @@
 <?php
     require_once('config.php');
 
-    $root = new Usuario();
-    $root->loadById(3);
-    
-    echo $root;
+    //Carrega um usuário
+    // $root = new Usuario();
+    // $root->loadById(3);
+    // echo $root;
+
+    //Carrega uma lista de usuário
+    // $lista = Usuario::getList();
+    // echo json_encode($lista);
+
+    //Carrega uma lista de usuários buscando pelo login
+    // $search = Usuario::search("ro");
+    // echo json_encode($search);
+
+    //Carrega um usuário usando o login e a senha
+    $usuario = new Usuario();
+    $usuario->login("root","12345");
+
+    echo $usuario;
 ?>
