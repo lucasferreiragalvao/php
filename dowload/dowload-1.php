7d3be3e777279192f2960ad2341226f3f81365bd
= <=lucasferreiragalvaoti@gmail.com>
2019-02-18 21:39:21 -0300

    Dowload PHP

---
+++ dowload/dowload-1.php
@@ -0,0 +1,20 @@
+<?php
+
+    $link = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
+
+    $content = file_get_contents($link);
+
+    $parse = parse_url($link);
+
+    $basename = basename($parse["path"]);
+
+    $file = fopen($basename, "w+");
+
+    fwrite($file,$content);
+
+    fclose($file);
+
+?>
+
+<!-- O comando igual substitui o comando echo !-->
+<img src="<?=$basename?>">
