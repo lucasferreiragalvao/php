605b2e5ace36985b20c0a2fa4eb9f88a31c72b12
= <=lucasferreiragalvaoti@gmail.com>
2019-02-20 23:31:47 -0300

    Rain Tpl

---
+++ tpl/index.php
@@ -0,0 +1,20 @@
+<?php
+    require_once("vendor/autoload.php");
+
+    use Rain\Tpl;
+    // config
+    $config = array(
+        "tpl_dir"       => "tpl/",
+        "cache_dir"     => "cache/"
+    );
+    Tpl::configure( $config );
+    // create the Tpl object
+    $tpl = new Tpl;
+    // assign a variable
+    $tpl->assign( "name", "Teste" );
+    // assign an array
+    $tpl->assign( "version", PHP_VERSION);
+    // draw the template
+    $tpl->draw( "index" );
+
+?>
