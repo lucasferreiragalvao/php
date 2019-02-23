0d116b6284b05b2a7892c10905d87d5ecc683ee2
= <=lucasferreiragalvaoti@gmail.com>
2019-02-23 16:53:55 -0300

    Criptografia PHP

---
+++ seguranca/openssl.php
@@ -0,0 +1,24 @@
+<?php
+    //Substitui a função mcrypt a partir do php 7.1.0
+
+    define('SECRET_IV', pack('a16', 'senha'));
+    define('SECRET', pack('a16', 'senha'));
+
+    $data = [
+        "nome" => "Teste"
+    ];
+
+    $openssl = openssl_encrypt(
+        json_encode($data),
+        'AES-128-CBC',
+        SECRET,
+        0,
+        SECRET_IV
+    );
+
+    echo $openssl;
+
+    $string = openssl_decrypt($openssl, 'AES-128-CBC', SECRET, 0 , SECRET_IV );
+
+    var_dump(json_decode($string,true));
+?>
